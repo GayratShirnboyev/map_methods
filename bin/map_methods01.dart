24fd7a66eb3c8807e7fb4fa8b329309e6e6cd55d
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-27 17:06:27 +0500

    sdfsdf1

--- bin/map_methods01.dart
+++ bin/map_methods01.dart
@@ -3,7 +3,7 @@ In the map where the properties of the car are given and you are also given a li
 there are two elements in it, add the first element of the list as a key and the second element as a value to the map.*/
 
 Map func(Map data, List lst) {
-  data['price'] = "50000";
+  data['price'] = 50000;
   return data;
 }
 
