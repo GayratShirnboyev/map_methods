a338eb93dbc728d7377b544c150ead076c77a536
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-27 23:33:19 +0500

    sfsdff2

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -2,11 +2,11 @@
 Car details will be given to you in map view, change the car price for it
 */
 
-Map func(Map data, List lst) {
-  data[lst[0]] = lst[1];
+Map func(Map data, List cost) {
+  data[cost[0]] = cost[1];
   return data;
 }
 
 void main() {
-  print(func({'name': "Mustang", 'color': "silwer"}, ['price', 80000]));
+  print(func({'name': "Mustang", 'color': "silwer"}, ["price", 800000000]));
 }
