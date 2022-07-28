e3bf194571ccdd57533f8624da342adb47fe89e3
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-28 10:28:39 +0500

    sergsr3

--- bin/map_methods05.dart
+++ bin/map_methods05.dart
@@ -5,12 +5,12 @@ add "@" to it.
 
 //your code here
 Map func(Map data) {
+  data.remove('surname');
   data['last_name'] = 'Kerry';
+
   return data;
 }
 
 void main() {
-  print(func({
-    'name': "John",
-  }));
+  print(func({'name': "John", "surname": "Kerry"}));
 }
